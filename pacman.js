3e0e6dbd55c5121db7051e273810ae9dc548cc37
Stephen Johnson <stevej@ucla.edu>
2018-08-22 12:22:36 -0700

    Main dependency chain creation loop

--- pacman.js
+++ pacman.js
@@ -7,3 +7,28 @@ var args = process.argv.slice(2)
 
 
 var dependencyChains = [];
+
+args.forEach(function (val, index, array) {
+  var dependencyPair = val.split(':');
+  // console.log(dependencyPair);
+
+  var foundMatch = false;
+  dependencyChains.forEach(function(dependencyChain) {
+    var firstDependency = dependencyChain[0];
+    var lastDependency = dependencyChain[dependencyChain.length - 1];
+    if(lastDependency == dependencyPair[0]) {
+      foundMatch = true;
+      if(dependencyPair[1] == " ") {
+        return;
+      }
+      if(firstDependency == dependencyPair[1]) {
+        throw new Error("Error: Dependencies create circular dependency for " + firstDependency);
+      }
+      dependencyChain.push(dependencyPair[1]);
+    }
+  });
+  if(!foundMatch) {
+    dependencyChains.push(dependencyPair);
+  }
+
+});
